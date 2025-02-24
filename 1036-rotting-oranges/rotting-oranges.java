d7bccc48c9b142466f4f36150ea421c674184e57
kanchanapallyswamy <kanchanapallyswamy@gmail.com>
2025-02-24 19:10:13 +0530

    Time: 2 ms (70.98%) | Memory: 42.9 MB (15.79%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -9,17 +9,17 @@ class Solution {
     Queue < Pair > q = new LinkedList < > ();
     int r = a.length, c = a[0].length;
     boolean visited[][] = new boolean[r][c];
-    int k1 = 0, fc = 0;
+    int k1 = 0, fc = 0 , l=0;
     for (int i = 0; i < r; i++) {
       for (int j = 0; j < c; j++) {
         if (a[i][j] == 2) {
           q.add(new Pair(i, j));
           visited[i][j] = true;
-        } else if (a[i][j] == 1) fc++;
+        } 
+        else if(a[i][j]==0)l++;
       }
     }
-    if (fc == 0) return 0;
-    if (q.size() == 0) return -1;
+    if(l==r*c)return 0;
     int cnt = -1;
     while (q.size() > 0) {
       int n = q.size();
@@ -27,19 +27,22 @@ class Solution {
       while (n--> 0) {
         var e = q.poll();
         int ni = e.i, nj = e.j;
-        // visited[ni][nj]=true;
         for (int k = 0; k < 4; k++) {
           int y = ni + di[k], z = nj + dj[k];
           if (isValid(r, c, y, z) && !visited[y][z] && a[y][z] == 1) {
             a[y][z] = 2;
             q.add(new Pair(y, z));
             visited[y][z] = true;
-            fc--;
           }
         }
       }
     }
-    return fc == 0 ? cnt : -1;
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            if(a[i][j]==1)fc++;
+        }
+    }
+    return fc >=1 ? -1 : cnt ;
   }
   boolean isValid(int r, int c, int i, int j) {
     return (i >= 0 && j >= 0 && i < r && j < c);
