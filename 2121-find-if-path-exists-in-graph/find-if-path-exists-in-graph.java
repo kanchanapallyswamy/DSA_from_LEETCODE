4effd93664906c2f56cdd5d76cec21da38de0f1a
kanchanapallyswamy <kanchanapallyswamy@gmail.com>
2025-03-01 10:20:55 +0530

    Time: 18 ms (91.03%) | Memory: 124.2 MB (94.55%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,37 +1,33 @@
 class Solution {
-  public boolean validPath(int n, int[][] edges, int s, int d) {
-    List < Integer > l = new ArrayList < > ();
-    Map < Integer, List < Integer >> mp = new HashMap < > ();
-    for (int i = 0; i < n; i++) mp.put(i, new ArrayList < > ());
-    for (int i = 0; i < edges.length; i++) {
-      int u = edges[i][0], v = edges[i][1];
-      mp.get(u).add(v);
-      mp.get(v).add(u);
-    }
-    int p[] = new int[n];
-    Arrays.fill(p, -1);
-    Queue < Integer > q = new LinkedList < > ();
-    Set < Integer > st = new HashSet < > ();
-    q.add(s);
-    st.add(s);
-    while (q.size() > 0) {
-      int u = q.remove();
-      for (var v: mp.get(u)) {
-        if (!st.contains(v)) {
-          st.add(v);
-          q.add(v);
-          p[v] = u;
+    public boolean validPath(int n, int[][] edges, int s, int d) {
+            UFDS ss=new UFDS(n);
+        for(int i=0;i<edges.length;i++){
+            int u=edges[i][0],v=edges[i][1];
+           ss. merge(u,v);
         }
-      }
-
+        return ss.find(s)==ss.find(d);
     }
-    while (d != -1) {
-      l.add(d);
-      d = p[d];
-    }
-    if (!l.contains(s)) return false;
-    // for(var i:p)
-    // System.out.print(i+" ");
-    return true;
-  }
+   
 }
+ class UFDS{
+    int n;
+    int [] parent;
+    public UFDS(int n){
+        this.n=n;
+        parent=new int[n];
+        for(int i=0;i<n;i++)parent[i]=i;
+        }
+
+        int find(int x){
+            if(x==parent[x]) return x;
+            return parent[x] = find(parent[x]); // path compression
+        }
+        void merge(int x,int y){
+                int leader_x=find(x);
+                int leader_y=find(y);
+                if(leader_x==leader_y)return; // already merged
+                parent[leader_y]=leader_x; // can optimize by "union by size"
+
+        }
+}
+
