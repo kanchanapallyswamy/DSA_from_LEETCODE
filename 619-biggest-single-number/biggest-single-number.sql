f0066512080bd13b19ca54217e372f64d3ba7a8d
kanchanapallyswamy <kanchanapallyswamy@gmail.com>
2025-03-15 20:37:34 +0530

    Time: 595 ms (78.52%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
-select  max(num) as num from mynumbers where num in (select num from mynumbers 
+select  coalesce ( (select  num  from mynumbers 
 group by num
-having count(num)=1)
+having count(num)=1 
+order by num desc limit 1) ,null ) as num;
