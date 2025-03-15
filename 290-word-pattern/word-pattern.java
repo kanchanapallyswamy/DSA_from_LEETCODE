5f60a7bb87199a42c994faf739e929058fc90fca
kanchanapallyswamy <kanchanapallyswamy@gmail.com>
2025-03-15 22:21:28 +0530

    Time: 2 ms (11.93%) | Memory: 41.6 MB (48.42%) - LeetSync

---
+++ 290-word-pattern/word-pattern.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean wordPattern(String s, String t) {
+        String a[]=t.split(" ");
+        Map<String,Character> mp1=new HashMap<>();
+        Map<Character,String> mp=new HashMap<>();
+        if(s.length()!=a.length)return false;
+        for(var i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            String ch1=a[i];
+            if(!mp.containsKey(ch))mp.put(ch,ch1);
+            else if(!mp.get(ch).equals(ch1)){
+                return false;
+            }
+        }
+        System.out.println(mp);
+        for(var i=0;i<a.length;i++){
+           String ch=a[i];
+           char ch1=s.charAt(i);
+            if(!mp1.containsKey(ch))mp1.put(ch,ch1);
+            else if(!mp1.get(ch).equals(ch1))return false;
+        }
+        System.out.println(mp1);
+        // System.out.println();
+        return true;
+    }
+}
