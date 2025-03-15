45c13a195f43dff49fb541ad5a5a779fd642e924
kanchanapallyswamy <kanchanapallyswamy@gmail.com>
2025-03-15 20:32:51 +0530

    Time: 661 ms (67.16%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,2 +1,4 @@
-select max(num) as num from mynumbers where num in
-(select num from mynumbers group by num having count(num) =1 )
+# Write your MySQL query statement below
+select  max(num) as num from mynumbers where num in (select num from mynumbers 
+group by num
+having count(num)=1)
