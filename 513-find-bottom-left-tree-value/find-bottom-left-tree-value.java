09c906d92364d267b2a5363b65064373602f191d
kanchanapallyswamy <kanchanapallyswamy@gmail.com>
2025-04-04 20:08:51 +0530

    Time: 9 ms (9.22%) | Memory: 47 MB (5.89%) - LeetSync

---
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+    if(root == null)return 0;
+        Map<Integer,List<Integer>> map  = new HashMap<>();
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(root,0));
+        map.put(0,new ArrayList<>());
+        map.get(0).add(root.val);
+        int max = 0;
+        while(!q.isEmpty()){
+            Pair f = q.poll();
+            int depth = f.depth+1;
+            if(f.node.left != null){
+                q.add(new Pair(f.node.left,depth));
+                map.putIfAbsent(depth,new ArrayList<>());
+                map.get(depth).add(f.node.left.val);
+                max = Math.max(max,depth);
+            }
+            if(f.node.right != null){
+                q.add(new Pair(f.node.right,depth));
+                map.putIfAbsent(depth,new ArrayList<>());
+                map.get(depth).add(f.node.right.val);
+                max = Math.max(max,depth);
+            }
+        }
+        return map.get(max).get(0);
+    }
+    class Pair{
+        TreeNode node;
+        int depth;
+        public Pair(TreeNode node,int depth){
+            this.depth = depth;
+            this.node = node;
+        }
+    }
+}
