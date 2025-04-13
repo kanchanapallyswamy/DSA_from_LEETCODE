87e74b13f5a995d51318adeeef2bfe8209c6a1c6
swamy_kanchanapally <kanchanapallyswamy@gmail.com>
2025-04-13 21:19:12 +0530

    Time: 10 ms (30.57%) | Memory: 45.5 MB (5.24%) - LeetSync

--- 1044-find-common-characters/find-common-characters.java
+++ 1044-find-common-characters/find-common-characters.java
@@ -1,37 +1,24 @@
-import java.util.*;
-
 class Solution {
     public List<String> commonChars(String[] words) {
-        Map<Character, Integer> mp = new HashMap<>();
+        Map<Character,Integer> mp = new HashMap<>();
         List<String> l = new ArrayList<>();
-
-        // Count frequency of each character in the first word
-        for (char p : words[0].toCharArray()) {
-            mp.put(p, 1 + mp.getOrDefault(p, 0));
-        }
-
-        // Store unique characters from the first word
+        for(var p : words[0].toCharArray())mp.put(p,1+mp.getOrDefault(p,0));
         Set<Character> st = new HashSet<>();
-        for (char i : words[0].toCharArray()) {
-            st.add(i);
-        }
-
-        // Check each character for minimum frequency across all words
-        for (char i : st) {
-            int minFreq = mp.get(i);
-            for (int k = 1; k < words.length; k++) {
-                int freq = 0;
-                for (char ch : words[k].toCharArray()) {
-                    if (ch == i) freq++;
+        for(var i : words[0].toCharArray())st.add(i);
+        for(var i : st){
+                    int cnt =check(i , mp.get(i) , words);
+                    while(cnt-->0)l.add(i+"");
                 }
-                minFreq = Math.min(minFreq, freq);
+                return l;
+    }
+    int check(char c , int min , String [] words){
+        for(int i = 1; i < words.length; i++){
+            int cnt = 0;
+            for(var j : words[i].toCharArray()){
+                if(j == c)cnt++;
             }
-            // If character appears in all words (minFreq > 0), add to result
-            while (minFreq-- > 0) {
-                l.add(i + "");
-            }
+            min = Math.min(min ,cnt);
         }
-
-        return l;
+        return min;
     }
 }
